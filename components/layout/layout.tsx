76f3a8c977753e5b8bd1bd3641cc38554fadcfec
Warwick Leahy [SSW] <warwickleahy@ssw.com.au>
2025-05-18 21:04:07 +1000

    Add analytics

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,5 +1,6 @@
 import React, { PropsWithChildren } from "react";
 import { LayoutProvider } from "./layout-context";
+import { Analytics } from '@vercel/analytics/react'
 import client from "../../tina/__generated__/client";
 import Header from "../nav/header";
 import Footer from "../nav/footer";
@@ -16,8 +17,10 @@ export default async function Layout({ children, rawPageData }: LayoutProps) {
 
   return (
     <LayoutProvider globalSettings={globalData.global} pageData={rawPageData}>
+      <Analytics />
       <Header />
       <main
+      
         className={cn(
           "font-sans flex-1 text-gray-800 bg-gradient-to-br from-white to-gray-50 dark:from-gray-900 dark:to-gray-1000 flex flex-col"
         )}
